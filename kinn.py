1c716d17f0339d2b23471e26d7d718b09a1fc9ea
unnamed-aka <167620397+unnamed-aka@users.noreply.github.com>
2024-04-23 16:32:15 +0300

    Kinn source code

--- kinn.py
+++ kinn.py
@@ -1,32 +1,71 @@
-import os,os.path
-import getpass
-import requests
+import subprocess
+import os, os.path
 import wget
+import getpass
+import PySimpleGUI as sg
 
-# Find username
-
+i = None
+pinglist = []
 user = getpass.getuser()
 
+sg.theme('light gray 3')
+
+layout = [
+    
+    [sg.Text('Set path to install file for mod install')],
+    [sg.InputText(key='path'), sg.FileBrowse(key='path')],
+    [sg.Output(size=(50,15))],
+    [sg.Button('Install')]
+    
+    ]
+
+
+window = sg.Window('Kinn', layout, icon='icon.ico')
+
 while True:
-    # User input for select and install mod
-    modsel = input('Enter mod name for automatic install: ').lower()
+    event, values = window.read()
     
-    # Check mod exist and index 
-    url1 = f'https://raw.githubusercontent.com/unnamed-aka/mod-index/main/{modsel}.txt'
-    if (requests.get(url1)).status_code == 404:
-        print('Error: mod not exists')
-        continue
-    else:
-        # Get mod index
-        modindex = (requests.get(url1)).text
-        
-        # Delete old mod if exists
-        if os.path.exists(f'C:/Users/{user}/AppData/Roaming/KinitoPET/mods/{modindex}'):
-            os.remove(f'C:/Users/{user}/AppData/Roaming/KinitoPET/mods/{modindex}')
-        
-        # Download and install mod
-        url = f'https://raw.githubusercontent.com/unnamed-aka/mod-repo/main/{modindex}'
-        wget.download(url,out = f'C:/Users/{user}/AppData/Roaming/KinitoPET/mods')
-        print('\nDone')
-    
-    
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'Install':
+        try:
+            file = open(values['path'], 'r')
+        except:
+            print('Cant open file')
+        else:
+            print('Read file...')
+            try:
+                info = (file.read()).split('|')
+                info[1] = info[1].split(',')
+                print(info)
+            except:
+                print('Wrong file format')
+                continue
+            else:
+                i = 0
+                print('Request host...')
+                while True:
+                    try:
+                        ping = (subprocess.run(['ping', '-c', '1', f'{info[0]}{info[1][i]}'], stdout=subprocess.PIPE)).returncode
+                        pinglist.extend(ping)
+                        i += 1
+                    except:
+                        break
+                if sum(pinglist) != 0:
+                    print('Cant connect with host')
+                    continue
+                else:
+                    i = 0
+                    while True:
+                        try:
+                            if os.path.exists(f'C:/Users/{user}/AppData/Roaming/KinitoPET/mods/{info[1][i]}'):
+                                os.remove(f'C:/Users/{user}/AppData/Roaming/KinitoPET/mods/{info[1][i]}')
+                            print(f'Downloading {info[1][i]}...')
+                            wget.download(f'{info[0]}{info[1][i]}', out=f'C:/Users/{user}/AppData/Roaming/KinitoPET/mods')
+                            i += 1
+                        except:
+                            print('Done')
+                            break
+                        continue
+                        
+         
